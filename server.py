ba623016e07eb1f390ed738fcaedfd760d48cbe7
LAWRANCE MAVIMBELA <mavimbelalawrence2004@gmail.com>
2026-05-02 18:29:13 +0200

    still debugging

--- server.py
+++ server.py
@@ -70,6 +70,7 @@ SUBJECT_ICONS = {
 # ======================
 
 class User(db.Model):
+    __tablename__="users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)

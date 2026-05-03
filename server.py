117a2132a65b425788c66036ec1f08ff07e3f519
LAWRANCE MAVIMBELA <mavimbelalawrence2004@gmail.com>
2026-05-03 23:30:17 +0200

    fix: flask 3 db init

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # ✅ CREATE TABLES ON START (SAFE)
-@app.before_first_request
+@app.before_request
 def create_tables():
     db.create_all()
 
